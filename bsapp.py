71f43812f996cba1f92f327c744cbcb319732462
BSGuoc <127440159+BSGuoc@users.noreply.github.com>
2024-01-13 12:19:44 +0100

    Update bsapp.py

--- bsapp.py
+++ bsapp.py
@@ -33,8 +33,8 @@ def predecir_imagen(imagen):
     
     # modelo_local = 'modelo.h5'
     ## gdown.download(modelo_url, modelo_local, quiet=False)
-    global load_model
-    model = load_model(modelo_local)
+    #global load_model
+   # model = load_model(response)
 
 # Resto del código para la predicción...
 
